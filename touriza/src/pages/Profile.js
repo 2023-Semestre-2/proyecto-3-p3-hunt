7712c2e575654d51205dfa254ff7f749d94461f5
JPHuntV <jeanpaulhunt16@gmail.com>
2023-11-13 19:44:39 -0600

    perfil css

--- touriza/src/pages/Profile.js
+++ touriza/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import { id } from "date-fns/locale";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 function Profile() {
@@ -14,39 +14,81 @@ function Profile() {
                 "Content-Type": "application/json",
             },
         })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            setUserInformation(data.user);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setUserInformation(data.user);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
-    , [idUser]);
+        , [idUser]);
 
 
     return (
-        <div>
-            <h1>Profile</h1>
-            <img 
-                style={{width:'70px'}}
-                src={`${process.env.PUBLIC_URL}/uploads/pfp/${userInformation.profilePicture}`} 
-                alt="profile picture"
-            />
-            {idUser == JSON.parse(localStorage.getItem('user')).idUser ? (
-                <button
-                    onClick={() => {navigate(`/EditProfile`);}}
-                >Editar</button>
-            ) : (
-                <></>
-            )}
-            <p>Posts: {userInformation.numberOfPost}</p>
-            <p>Favoritos: {userInformation.numberOfFavorites}</p>
-            <p>Comentarios: {userInformation.numberOfComments}</p>
-            <p>{userInformation.name +" "+ userInformation.lastName}</p>
-            <p>{userInformation.email}</p>
-            <p>{userInformation.phone}</p>
+        <div className="Profile-page">
+            <div className="profile-container">
+                <div className="profile-info">
+                <img
+                    className="profile-picture"
+                    src={`${process.env.PUBLIC_URL}/uploads/pfp/${userInformation.profilePicture}`}
+                    alt="profile picture"
+                />
+                    <div className="profile-header">
+                    {idUser == JSON.parse(localStorage.getItem('user')).idUser ? (
+                        <button
+                            className="btn btn-primary edit-profile"
+                            onClick={() => { navigate(`/EditProfile`); }}
+                        >Editar perfil</button>
+                    ) : (
+                        <></>
+                    )}
+                    </div>
+                    <div className="profile-body">
+                    <div className="profile-stats">
+                        <div className="stat-group">
+                            <label>
+                                {
+                                userInformation.numberOfPost?
+                                userInformation.numberOfPost
+                                : 0
+                                }
+                            </label>    
+                            
+                            <label>Posts</label>
+                                
+
+                        </div>
+                        <div className="stat-group">
+                            <label>
+                                {
+                                userInformation.numberOfComments?
+                                userInformation.numberOfComments
+                                : 0
+                                }
+                            </label>
+                            <label>Comentarios</label>
+                        </div>
+                        <div className="stat-group">
+                            <label>
+                                {
+                                userInformation.numberOfFavorites?
+                                userInformation.numberOfFavorites
+                                : 0
+                                }
+                            </label>
+                            <label>Favoritos</label>
+                        </div>
+                    </div>
+                    <h1>
+                        {userInformation.name + " " + userInformation.lastName}
+                    </h1>
+                    <p>{userInformation.email}</p>
+                    <p>{userInformation.phone}</p>
+                </div>
+                </div>
+            </div>
 
         </div>
     );
